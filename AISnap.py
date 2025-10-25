bb551b889c67c312d6601d0a7c769d9e546c9bcd
= <=>
2025-10-26 06:48:30 +0700

    first commit

---
+++ AISnap.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import google.generativeai as ai
+st.title("AI Snap - Kelompok 1")
+foto = st.camera_input("Ambil gambar")
+if foto:
+    ai.configure(api_key="AIzaSyBr-J4TSOi1R40ia7vr1C9IxTAgFsqDIYw")
+    model = ai.GenerativeModel('gemini-2.0-flash')
+    prompt = model.generate_content([
+            "Jelaskan detail object yang dipegang oleh orang di gambar",
+            {
+                "data": foto.getvalue(),
+                "mime_type": "image/jpeg"
+            }
+        ]
+    )
+    st.write(prompt.text)
